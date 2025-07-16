48fea18abf1542106b1e01b2969cebb595197344
AM0129 <ajmalinis@mymail.mapua.edu.ph>
2025-07-16 16:29:04 +0800

    Update Store.cpp

--- Store.cpp
+++ Store.cpp
@@ -1,1 +1,21 @@
+#include <fstream>
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+int main() 
+{
+    std::string name = "item1";
+    int value = 100;
+
+    std::ofstream file("items.csv", std::ios::app); // 'app' adds at the end
+    if (file.is_open()) {
+        file << name << "," << value << "\n";
+        file.close();
+    } else {
+        std::cout << "Unable to open file\n";
+    }
+
+    return 0;
+}
