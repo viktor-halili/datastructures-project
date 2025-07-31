abea3e190577a1336955d74ecbae18ce06881c72
ThePhotonTech1 <78462949+ThePhotonTech1@users.noreply.github.com>
2025-07-31 10:08:19 +0800

    Update mainProgram.cpp
    working display and read functions

--- mainProgram.cpp
+++ mainProgram.cpp
@@ -1,44 +1,66 @@
 #include <iostream>
 #include <string>
 #include "login.cpp"
+#include "Reader.cpp"
+
 
 using namespace std;
+vector<MissingItem> itemsReaded;
+
 void adminmenu(){
     int adminSelect;
-    cout <<"Welcome Admin!\n"<< "Enter 1 to add new item, Enter 2 to claim, 3 to view items, 4 to add new user, 5 to logout: ";
-    cin >> adminSelect;
-    switch (adminSelect){
-        case 1: //Add new item function here
-            break;
-        case 2: //Add claim item function
-            break;
-        case 3: //Add view item function
-            break;
-        case 4: //Add new user function
-            break;
-        case 5: //Logout
-            cout << "Logging out...\n";
-            break;
+    string adminTransact = "N";
+    
+    itemsReaded = itemRead();
+    do {
+        cout <<"Welcome Admin!\n"<< "Enter 1 to add new item, Enter 2 to claim, 3 to view items, 4 to add new user, 5 to logout: ";
+        cin >> adminSelect;
+        switch (adminSelect){
+            case 1: //Add new item function here
+                break;
+            case 2: //Add claim item function
+                break;
+            case 3: //Add view item function
+                displayItems(itemsReaded);
+                break;
+            case 4: //Add new user function
+                break;
+            case 5: //Logout
+                cout << "Logging out...\n";
+                return;
+                
+            }
+        cout << "Would you like to have another transaction? (Y/N): ";
+        cin >> adminTransact;
+        cout << "---------------------------------------------------\n";
     }
-
+    while (adminTransact == "Y" || adminTransact == "y");
 }
 
 void studentmenu(){
     int studentSelect;
-    cout << "Enter 1 for next page, Enter 2 for search, Enter 3 to logout: ";
+    string studTransact = "N";
+    itemsReaded = itemRead();
+    displayItems(itemsReaded);
+    do {
+    cout << "Enter 1 for search, Enter 2 to logout: ";
     cin >> studentSelect;
     switch (studentSelect){
-        case 1://Enter function to next page
+        case 1://Enter function search
 
             break;
-        case 2://Enter function to search
-
-            break;
-        case 3://Logout
+        case 2://logout
             cout << "Logging out...\n";
-            break;
+            return;
+            
+        }
+        cout << "Would you like to have another transaction? (Y/N): ";
+        cin >> studTransact;
+        
     }
+    while (studTransact == "Y" || studTransact == "y");
 }
+
 int main(){
 bool endProgram = false;
 int loginselector;
